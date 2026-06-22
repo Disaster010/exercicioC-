b87a3fd5983e29fdcf3040fdab6a8211c5c16b44
Disaster010 <luangabrielf6@gmail.com>
2026-06-22 14:05:40 -0300

    CursoCSharp

--- ClassesEMetodos/Exercicio4.cs
+++ ClassesEMetodos/Exercicio4.cs
@@ -16,13 +16,25 @@ namespace CursoCSharp.ClassesEMetodos {
             Continente = continente;
             Titulos = titulos;
         }
-        public void GanharTitulo(double titulos) {
-            titulos += 1;
+        public void GanharTitulo(int numero) {
+            Titulos += numero;
+        }
+        public void PerderTitulo(int numero) {
+            Titulos -= numero;
+        }
+        public void MostrarDados() {
+            Console.WriteLine($"Seleção: {País} / Continente: {Continente} / Titulos: {Titulos} ");
         }
     }
 
     internal class Exercicio4 {
         public static void Executar() {
+
+            Selecao Selecao1 = new Selecao("Brasil","América do Sul",0);
+
+            Selecao1.GanharTitulo(5);
+            Selecao1.PerderTitulo(0);
+            Selecao1.MostrarDados();
 
         }
     }
